e06f7e9e63222a4e81a8d672e8ce8c51aee46708
dostonbokhodirov <boxodirov.doston@gmail.com>
2022-03-02 16:50:02 +0500

    initial commit

---
+++ src/main/java/uz/pdp/pdp_food_delivery/PdpFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.pdp_food_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpFoodDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdpFoodDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/uz/pdp/pdp_food_delivery/rest/entity/BaseEntity.java
@@ -0,0 +1,4 @@
+package uz.pdp.pdp_food_delivery.rest.entity;
+
+public interface BaseEntity {
+}
