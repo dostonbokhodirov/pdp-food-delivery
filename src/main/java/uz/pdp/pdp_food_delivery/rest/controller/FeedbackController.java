0b5386b5ac52ef21122529142d2834a4410a8da3
Mr.Akmal <mr.akmal4555@mail.ru>
2022-03-03 04:49:41 +0300

    123

--- src/main/java/uz/pdp/pdp_food_delivery/rest/controller/FeedbackController.java
+++ src/main/java/uz/pdp/pdp_food_delivery/rest/controller/FeedbackController.java
@@ -1,13 +1,46 @@
 package uz.pdp.pdp_food_delivery.rest.controller;
 
+import org.springframework.web.bind.annotation.*;
 import uz.pdp.pdp_food_delivery.rest.controller.base.AbstractController;
+import uz.pdp.pdp_food_delivery.rest.dto.feedback.FeedbackCreateDto;
+import uz.pdp.pdp_food_delivery.rest.dto.feedback.FeedbackUpdateDto;
 import uz.pdp.pdp_food_delivery.rest.service.feedback.FeedbackService;
 
+
+@RestController
+@RequestMapping("/feedback")
 public class FeedbackController extends AbstractController<FeedbackService> {
 
     public FeedbackController(FeedbackService service) {
         super(service);
     }
 
+    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    public void create(@RequestBody FeedbackCreateDto dto){
+        service.create(dto);
+    }
 
+    @RequestMapping(value = "/update",method = RequestMethod.POST)
+    public String update(@RequestBody FeedbackUpdateDto dto){
+            service.update(dto);
+            return null;
+    }
+
+    @RequestMapping(value = "/delete/{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Long id){
+            service.delete(id);
+    }
+
+
+    @RequestMapping(value = "/detail/{id}",method = RequestMethod.GET)
+    public String get(@PathVariable("id") Long id){
+            service.get(id);
+            return null;
+    }
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public String getAll(){
+            service.getAll();
+            return null;
+    }
 }
--- src/main/java/uz/pdp/pdp_food_delivery/rest/mapper/feedback/FeedbackMapper.java
+++ src/main/java/uz/pdp/pdp_food_delivery/rest/mapper/feedback/FeedbackMapper.java
@@ -2,10 +2,23 @@ package uz.pdp.pdp_food_delivery.rest.mapper.feedback;
 
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
+import uz.pdp.pdp_food_delivery.rest.dto.feedback.FeedbackCreateDto;
+import uz.pdp.pdp_food_delivery.rest.dto.feedback.FeedbackDto;
+import uz.pdp.pdp_food_delivery.rest.dto.feedback.FeedbackUpdateDto;
+import uz.pdp.pdp_food_delivery.rest.entity.Feedback;
 import uz.pdp.pdp_food_delivery.rest.mapper.BaseMapper;
+
+import java.util.List;
 
 
 @Component
 @Mapper(componentModel = "spring")
 public interface FeedbackMapper extends BaseMapper {
+
+    Feedback fromCreateDto(FeedbackCreateDto feedbackCreateDto);
+
+    Feedback fromUpdateDto(FeedbackUpdateDto feedbackUpdateDto);
+
+    List<FeedbackDto> toDto(List<Feedback> feedback);
+    FeedbackDto toDto(Feedback feedback);
 }
